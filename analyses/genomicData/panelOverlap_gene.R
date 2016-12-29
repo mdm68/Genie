88a238d831e039b0d298915eb25579ecdeb62a24
alexbaras <baras@jhmi.edu>
2016-12-29 01:28:05 -0500

    Update panelOverlap_gene.R

--- analyses/genomicData/panelOverlap_gene.R
+++ analyses/genomicData/panelOverlap_gene.R
@@ -8,14 +8,18 @@ synapseLogin(username=,password=) # you could do this with prompt or coded, but 
 genieClinData = read.delim(synGet("syn7871792")@filePath,header=TRUE)
 genieBedData = read.delim(synGet("syn7871816")@filePath,header=TRUE)
 
-# get unique SEQ_ASSAY_ID from clinical file
-panels = as.character(unique(genieClinData$SEQ_ASSAY_ID))
+# get unique SEQ_ASSAY_ID from clinical file and unique genes covered
+panels = aggregate(Hugo_Symbol ~ SEQ_ASSAY_ID,genieBedData[genieBedData$SEQ_ASSAY_ID %in% unique(genieClinData$SEQ_ASSAY_ID),],function(x) {length(unique(x))})
+panels = panels[order(panels$Hugo_Symbol),]
 # preallocate matrix
-panelGenes = matrix(NA,length(panels),length(panels),dimnames=list(panels,panels))
+panelGenes = matrix(NA,nrow(panels),nrow(panels),dimnames=list(panels$SEQ_ASSAY_ID,panels$SEQ_ASSAY_ID))
 
 # find intersect of unique genes 
-for (i in 1:length(panels)) {
-  for (j in 1:length(panels)) {
-    panelGenes[i,j] = length(intersect(unique(as.character(genieBedData$Hugo_Symbol[genieBedData$SEQ_ASSAY_ID==panels[i]])),unique(as.character(genieBedData$Hugo_Symbol[genieBedData$SEQ_ASSAY_ID==panels[j]]))))
+for (i in 1:nrow(panels)) {
+  for (j in 1:nrow(panels)) {
+    panelGenes[i,j] = length(intersect(unique(as.character(genieBedData$Hugo_Symbol[genieBedData$SEQ_ASSAY_ID==panels$SEQ_ASSAY_ID[i]])),unique(as.character(genieBedData$Hugo_Symbol[genieBedData$SEQ_ASSAY_ID==panels$SEQ_ASSAY_ID[j]]))))
   }
 }
+
+# write to file
+write.table(panelGenes,"~/Desktop/out.csv")
